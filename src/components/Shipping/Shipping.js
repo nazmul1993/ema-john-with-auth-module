edf0a549112281d3ac22714da2a0abd992b7f581
nazmul1993 <md.islam.nazmul@stud.uni-due.de>
2021-11-09 16:06:33 +0100

    final commit

--- src/components/Shipping/Shipping.js
+++ src/components/Shipping/Shipping.js
@@ -1,30 +1,62 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import useAuth from '../../hooks/useAuth';
+import { clearTheCart, getStoredCart } from '../../utilities/fakedb';
 import './Shipping.css';
 
 const Shipping = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const { user } = useAuth();
-    const onSubmit = data => {
-        console.log(data)
-    };
-    return (
-        <div>
-            <form className="shipping-form" onSubmit={handleSubmit(onSubmit)}>
+        const { register, handleSubmit, reset, formState: { errors } } = useForm();
+        const { user } = useAuth();
+        const onSubmit = data => {
+            const savedCart = getStoredCart();
+            data.order = savedCart;
 
-                <input defaultValue={user.displayName} {...register("name")} />
+            fetch('http://localhost:5000/orders', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
+                .then(res => res.json())
+                .then(result => {
+                    if (result.insertedId) {
+                        alert('Order processed successfully')
+                        clearTheCart();
+                        reset();
+                    }
+                })
+        };
+        return ( <
+            div >
+            <
+            form className = "shipping-form"
+            onSubmit = { handleSubmit(onSubmit) } >
 
-                <input defaultValue={user.email} {...register("email", { required: true })} />
-                {errors.email && <span className="error">This field is required</span>}
-                <input placeholder="Address" defaultValue="" {...register("address")} />
-                <input placeholder="City" defaultValue="" {...register("city")} />
-                <input placeholder="phone number" defaultValue="" {...register("phone")} />
+            <
+            input defaultValue = { user.displayName } {...register("name") }
+            />
 
-                <input type="submit" />
-            </form>
-        </div>
-    );
-};
+            <
+            input defaultValue = { user.email } {...register("email", { required: true }) }
+            /> {
+                errors.email && < span className = "error" > This field is required < /span>} <
+                    input placeholder = "Address"
+                defaultValue = "" {...register("address") }
+                /> <
+                input placeholder = "City"
+                defaultValue = "" {...register("city") }
+                /> <
+                input placeholder = "phone number"
+                defaultValue = "" {...register("phone") }
+                />
 
-export default Shipping;
+                <
+                input type = "submit" / >
+                    <
+                    /form> <
+                    /div>
+            );
+        };
+
+        export default Shipping;
--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,27 +1,45 @@
 import { useState, useEffect } from 'react';
+import { visitFunctionBody } from 'typescript';
+import Product from '../components/Product/Product';
 import { getStoredCart } from '../utilities/fakedb';
 
-const useCart = products => {
+const useCart = () => {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
+        const savedCart = getStoredCart();
+        const keys = Object.keys(savedCart);
+        fetch('http://localhost:5000/bykeys', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(keys)
+            })
+            .then(res => res.json())
+            .then(products => {
+                console.log(products)
 
-        if (products.length) {
-            const savedCart = getStoredCart();
-            const storedCart = [];
-            for (const key in savedCart) {
-                const addedProduct = products.find(product => product.key === key);
-                if (addedProduct) {
-                    // set quantity
-                    const quantity = savedCart[key];
-                    addedProduct.quantity = quantity;
-                    storedCart.push(addedProduct);
+                if (products.length) {
+                    const savedCart = getStoredCart();
+                    const storedCart = [];
+                    for (const key in savedCart) {
+                        const addedProduct = products.find(product => product.key === key);
+                        if (addedProduct) {
+                            // set quantity
+                            const quantity = savedCart[key];
+                            addedProduct.quantity = quantity;
+                            storedCart.push(addedProduct);
+                        }
+                    }
+                    setCart(storedCart);
                 }
-            }
-            setCart(storedCart);
-        }
+            })
 
-    }, [products]);
+
+
+
+    }, []);
 
     return [cart, setCart];
 }
